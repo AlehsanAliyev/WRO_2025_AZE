d56b81207b30d679e72ab36161a225d837a28ee5
AlehsanAliyev <aleksvandijk4@gmail.com>
2025-09-26 12:01:57 +0400

    New car photos and team photos, round 1 added

--- codes/round1.ino
+++ codes/round1.ino
@@ -30,24 +30,22 @@ Servo steering;
 
 bool start = true;
 bool inparking = true;
-int lastvalue = 5;
-int speed = 120;
-int mid = 100;
+
+int distance;
+
+int speed = 180;
 int left = 75;
+int mid = 102;
 int right = 130;
 int turns = 0;
 int finish_timer = 0;
 int clockwise = 0;
 
-uint16_t distance_middle = 0;
-uint16_t distance_left = 0;
-uint16_t distance_right = 0;
-
 void setup() {
   Serial.begin(9600);
   steering.attach(servoPin);
   steering.write(mid);
-
+  distance = 50;
 
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -69,7 +67,7 @@ void setup() {
   delay(1000);
 }
 
-
+// void test(){
 void loop() {
   while(start){
     if(digitalRead(button) == HIGH){
@@ -81,36 +79,89 @@ void loop() {
     while(1){}
   }
   int d = direction();
-  if(d == 2 && clockwise != -1){
+  if(distance > 50 && d == 2 && clockwise != -1){
     clockwise = 1;
     steering.write(right);
-    forward(speed, 800);
-    steering.write(mid);
+    delay(20);
 
-    backward(speed, 800);
-    delay(100);
+    forward(120, 800);
+    steering.write(left);
+    delay(20);
+
+    backward(120, 800);
 
     steering.write(right);
-    forward(speed, 1500);
-    steering.write(mid);
+    delay(20);
+
+    forward(120, 1100);
+    steering.write(mid - 11);
+
     turns++;
+    distance = 0;
   }
-  else if (d == 1 && clockwise != 1){
+  else if (distance > 50 && d == 1 && clockwise != 1){
     clockwise = -1;
     steering.write(left);
-    forward(speed, 800);
-    steering.write(mid);
+    delay(20);
 
-    backward(speed, 800);
-    delay(100);
+    forward(120, 800);
+
+    steering.write(right);
+    delay(20);
+
+    backward(120, 800);
     
     steering.write(left);
-    forward(speed, 1500);
-    steering.write(mid);
+    delay(20);
+
+    forward(120, 1100);
+    steering.write(mid + 15);
+
     turns++;
+    distance = 0;
+  }
+  if(digitalRead(irleft) == LOW) {
+    if(digitalRead(irback) == LOW){
+      steering.write(right);
+      delay(20);
+
+      forward(speed, 500);
+      
+      steering.write(mid - 11);
+      delay(100);
+    }
+    else{
+      steering.write(left);
+      delay(20);
+
+      backward(speed, 800);
+      steering.write(mid + 15);
+      delay(100);
+    }
+  }
+  else if(digitalRead(irright) == LOW){
+    if(digitalRead(irback) == LOW){
+      steering.write(left);
+      delay(20);
+
+      forward(speed, 500);
+      
+      steering.write(mid + 15);
+      delay(100);
+    }
+    else{
+      steering.write(right);
+      delay(20);
+
+      backward(speed, 800);
+      steering.write(mid - 11);
+      delay(100);
+    }
   }
   else{
-    forward(speed,30);
+    distance++;
+    steering.write(mid);
+    forward(speed, 30);
   }
 }
 
@@ -171,17 +222,20 @@ void Stop() {
 
 
 
-
 // void loop(){
 int direction() {
+
+  digitalWrite(s2_pin,LOW);
+  digitalWrite(s3_pin,HIGH);
+  int blue  = pulseIn(out_pin, LOW);
+ blue = map(blue, 25,70,255,0);
+
 
   digitalWrite(s2_pin,LOW);
   digitalWrite(s3_pin,LOW);
   int red = pulseIn(out_pin, LOW);
  red = map(red, 25,72,255,0);
 
-  //Serial.print(red);
-  Serial.print(" ");
 
 
   digitalWrite(s2_pin,HIGH);
@@ -189,27 +243,31 @@ int direction() {
   int green = pulseIn(out_pin, LOW);
  green = map(green, 30,90,255,0);
 
-  //Serial.print(green);
-  Serial.print(" ");
+
+  // int sm = (red + green + blue);
+
+  // red = red * 1000 / sm;
+  // green = green * 1000 / sm;
+  // blue = blue * 1000 / sm;
+
+  // Serial.print(blue);
+  // Serial.print(" ");
+  
+  // Serial.print(red);
+  // Serial.print(" ");
+
+  // Serial.print(green);
+  // Serial.print(" ");
+
+   Serial.println("");
 
 
-  digitalWrite(s2_pin,LOW);
-  digitalWrite(s3_pin,HIGH);
-  int blue  = pulseIn(out_pin, LOW);
- blue = map(blue, 25,70,255,0);
-
-  //Serial.print(blue);
-  Serial.print(" ");
-
-
-  Serial.println("");
-
-  if(red < -3000){
+  if(red < -8000){
     Serial.print(1); // blue line - counter clock - left
-    Serial.print(" ");
+   Serial.print(" ");
     return 1;
   }
-  else if(blue < -2000){
+  else if(blue < -5000){
     Serial.print(2); // orange - clock - right
     Serial.print(" ");
     return 2;
